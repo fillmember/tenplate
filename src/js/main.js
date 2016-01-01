daad2c88137614127399b4d9c82335e399ce1664
FillMember <fillmember@gmail.com>
2016-01-02 00:47:08 +0100

    clean up dependencies and entry file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,1 @@
-var THREE = require('three')
-var $ = require('jquery')
-
-if (__DEV__) {
-
-var dami = require('./test')
-
-dami.mi()
-
-}
+__DEV__ && console.log("dev message!")
--- src/js/test.js
+++
@@ -1,4 +0,0 @@
-
-// var odds = evens.map(v => v + 1);
-
-module.exports = {mi:function(){console.log("mi~")}}
